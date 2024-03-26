6b9a90a11c48e849a77543245286dff2abf26396
Aditya Maurya <madityauma20@gmail.com>
2024-03-26 23:42:59 +0530

    Tomato Food Delivery React App

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import'./Footer.css'
+import { assets } from '../../assets/assets';
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi illo quis officia saepe provident ut, nostrum cum impedit omnis labore corrupti voluptas obcaecati aspernatur, voluptatem debitis illum nulla quod quibusdam?</p>
+                <div className="footer-social-icon">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+12-456-123-4567</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+        <p className='footer-copyright'>Copyright 2024. Tomato.com - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-content">
+            <h2>Order you favourite food here.</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vel atque molestiae ad nihil in enim, minima rerum earum, dignissimos nesciunt, nulla totam? Pariatur laudantium optio facere, alias iste aut.
+            </p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
